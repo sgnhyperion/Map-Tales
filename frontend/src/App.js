3e90b6861a2719d1a4f88fe816095b8d7193ee4c
Harsh <hk755063@gmail.com>
2024-11-25 20:57:06 +0530

    mapbox integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,34 @@
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: 77.6602,
+        latitude: 12.8452,
+        zoom: 7
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+    >
+      <Marker 
+        longitude={77.6602} 
+        latitude={12.8452} 
+        anchor="bottom"
+      >
+        <FaMapMarkerAlt 
+          style={{ 
+            width: '30px', 
+            height: '30px', 
+            color: "blue",
+          }}
+        />   
+      </Marker>
+    </Map>
   );
 }
 
