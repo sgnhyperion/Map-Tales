5dd240265ef57a027d78c672e7db75fd5b89e90b
Harsh <hk755063@gmail.com>
2024-11-27 00:22:32 +0530

    deployement bug fix

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const UsersRoute = require("./routes/users");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 
