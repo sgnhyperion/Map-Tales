68f3c7dab7f6520b899a7eea2d46721f06f72245
Harsh <hk755063@gmail.com>
2024-11-26 23:58:42 +0530

    deployement bug fix

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const UsersRoute = require("./routes/users");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://map-tales-frontend.vercel.app/" }));
 app.use('/uploads', express.static('uploads'));
 
 
